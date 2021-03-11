c29fce66166cd377016f00b381a399b0a7bb01e7
Vincent Breitmoser <look@my.amazin.horse>
2021-03-11 22:58:02 +0100

    add acceptance tests

---
+++ desec/resource_domain_test.go
@@ -0,0 +1,75 @@
+package desec
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	dsc "github.com/nrdcg/desec"
+)
+
+func TestAccDesecDomainBasic(t *testing.T) {
+	uuid, err := uuid.GenerateUUID()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	domainName := fmt.Sprintf("%s.example", uuid)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDesecDomainDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckDesecDomainConfigBasic(domainName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("desec_domain.desec-example", "name", domainName),
+					testAccCheckDesecDomainExists,
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDesecDomainDestroy(s *terraform.State) error {
+	c := testAccProvider.Meta().(*dsc.Client)
+
+	domains, err := c.Domains.GetAll()
+	if err != nil {
+		return err
+	}
+	for _, domain := range domains {
+		return fmt.Errorf("Domain still exists:\n%#v", domain.Name)
+	}
+
+	return nil
+}
+
+func testAccCheckDesecDomainConfigBasic(domainName string) string {
+	return fmt.Sprintf(`
+	resource "desec_domain" "desec-example" {
+		name = "%s"
+	}
+	`, domainName)
+}
+
+func testAccCheckDesecDomainExists(s *terraform.State) error {
+	c := testAccProvider.Meta().(*dsc.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "desec_domain" {
+			continue
+		}
+
+		_, err := c.Domains.Get(rs.Primary.ID)
+		if err != nil {
+			return nil
+		}
+	}
+
+	return nil
+}
---
+++ desec/resource_rrset_test.go
@@ -0,0 +1,78 @@
+package desec
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/go-uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	dsc "github.com/nrdcg/desec"
+)
+
+func TestAccDesecRRsetBasic(t *testing.T) {
+	uuid, err := uuid.GenerateUUID()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	domainName := fmt.Sprintf("%s.example", uuid)
+	name := fmt.Sprintf("test.%s.", domainName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDesecDomainDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckDesecRRsetConfigBasic(domainName, "[ \"127.0.0.1\" ]"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("desec_rrset.desec-example_hello_A", "name", name),
+					resource.TestCheckResourceAttr("desec_rrset.desec-example_hello_A", "records.#", "1"),
+					testAccCheckDesecRRsetExists,
+				),
+			},
+			{
+				Config: testAccCheckDesecRRsetConfigBasic(domainName, "[ \"127.0.0.1\", \"127.0.0.2\" ]"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("desec_rrset.desec-example_hello_A", "records.#", "2"),
+					testAccCheckDesecRRsetExists,
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDesecRRsetConfigBasic(domainName, ips string) string {
+	return fmt.Sprintf(`
+	resource "desec_domain" "desec-example" {
+		name = "%s"
+	}
+	resource "desec_rrset" "desec-example_hello_A" {
+		domain = desec_domain.desec-example.name
+		subname = "test"
+		type = "A"
+		records = %s
+		ttl = 3600
+	}
+	`, domainName, ips)
+}
+
+func testAccCheckDesecRRsetExists(s *terraform.State) error {
+	c := testAccProvider.Meta().(*dsc.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "desec_rrset" {
+			continue
+		}
+
+		domainName, subName, recordType, err := namesFromId(rs.Primary.ID)
+		_, err = c.Records.Get(domainName, subName, recordType)
+		if err != nil {
+			return nil
+		}
+	}
+
+	return nil
+}
