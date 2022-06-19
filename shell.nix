a15f849a7254d1e5cba62b47840424651bcb6027
Vincent Breitmoser <look@my.amazin.horse>
2022-06-20 01:03:44 +0200

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,5 @@
 let
-  pkgs = import (builtins.fetchTarball {
-    # Descriptive name to make the store path easier to identify
-    name = "nixos-21.05-2021-06-15";
-    # Commit hash for specific nixpkgs revision we pin to
-    url = "https://github.com/nixos/nixpkgs/archive/1f91fd1040667e9265a760b0347f8bc416249da7.tar.gz";
-    # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "sha256-krXZQ0lObduC95f40K3JwIT//VIBpXBwVNclqh5njtE=";
-  }) {
+  pkgs = import <nixpkgs> {
     overlays = [
       # for pinning specific versions (currently not needed)
       # (self: super: { nodejs = self.nodejs-12_x; })
@@ -16,6 +9,6 @@ in
   pkgs.mkShell {
     buildInputs = with pkgs; [
       go
-      terraform_0_13
+      terraform
     ];
   }
